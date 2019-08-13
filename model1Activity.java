43d759fe00f9640efe052142a5be18a514be3b53
Shilpi Priya <48611488+shilpipriya@users.noreply.github.com>
2019-08-13 21:01:17 +0530

    Add files via upload

---
+++ model1Activity.java
@@ -0,0 +1,13 @@
+package com.hackfest.kisancare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class model1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model1);
+    }
+}
